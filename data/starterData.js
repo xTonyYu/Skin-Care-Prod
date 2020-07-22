970fb3981f7b5beb15ed8dd3e1b84e5094ecfb39
xTonyYu <xtonyyu@gmail.com>
2020-07-22 03:22:16 -0700

    show page completed

---
+++ data/starterData.js
@@ -0,0 +1,28 @@
+const dataUsers = [
+    {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'whatever@no.com',
+    review: 'blah blah blah'
+    },
+    {
+    firstName: 'Susan',
+    lastName: 'Young',
+    email: 'SY@no.com',
+    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde labore obcaecati tempore non maxime vitae ad cumque id debitis.'
+    },
+    {
+    firstName: 'Darth',
+    lastName: 'Vader',
+    email: 'DV@deathstar.org',
+    review: 'THis product really help smooth my skin after years fighting with jedis.'
+    },
+    {
+    firstName: 'Nyota',
+    lastName: 'Uhura',
+    email: 'Uhura@ussenterprise.com',
+    review: 'blah blah blah I am already beautiful but still use this product'
+    },
+]
+
+module.exports = {dataUsers}
